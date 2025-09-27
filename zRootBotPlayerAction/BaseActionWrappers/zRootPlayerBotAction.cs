242c82f52baf24315ac51ba1e1016ef0b272072b
SlideDrum <slidedrumofficial@gmail.com>
2025-09-27 04:45:18 -0400

    Wrapper class works!  Now to re-make update!

---
+++ zRootBotPlayerAction/BaseActionWrappers/zRootPlayerBotAction.cs
@@ -0,0 +1,73 @@
+﻿using Player;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZombieTweak2.zRootBotPlayerAction.BaseActionWrappers
+{
+    internal class zRootPlayerBotAction : RootPlayerBotAction
+    {
+        public zRootPlayerBotAction(Descriptor desc) : base(desc)
+        {
+            var data = RootActionRewritePatch.GetOrCreateData(this);
+            //hurt action is handled somewhere else?  Might need to find it?  Or maybe it's unused now?  Who knows.
+
+            var m_idleAction = new zPlayerBotActionIdle.Descriptor(this.m_bot); // Create the action wrapper
+            m_idleAction.PrioFreezeForTwitcher = RootPlayerBotAction.m_prioSettings.IdleFreezeForTwitcher;
+            m_idleAction.PrioFreezeForInteraction = RootPlayerBotAction.m_prioSettings.IdleFreezeForInteraction;
+            m_idleAction.PrioLook = RootPlayerBotAction.m_prioSettings.IdleLook;
+            m_idleAction.PrioPrepareAction = RootPlayerBotAction.m_prioSettings.IdlePrepare;
+            this.m_idleAction = m_idleAction; // Assign it to the instance
+            data.allActions.Add(m_idleAction); // Add it to our custom list
+            // Repeat
+            var m_followLeaderAction = new zPlayerBotActionFollow.Descriptor(this.m_bot);
+            this.m_followLeaderAction = m_followLeaderAction;
+            data.allActions.Add(m_followLeaderAction);
+
+            var m_useBioscanAction = new zPlayerBotActionUseBioscan.Descriptor(this.m_bot);
+            this.m_useBioscanAction = m_useBioscanAction;
+            data.allActions.Add(m_useBioscanAction);
+
+            var m_attackAction = new zPlayerBotActionAttack.Descriptor(this.m_bot);
+            this.m_attackAction = m_attackAction;
+            data.allActions.Add(m_attackAction);
+
+            var m_reviveAction = new zPlayerBotActionRevive.Descriptor(this.m_bot);
+            this.m_reviveAction = m_reviveAction;
+            data.allActions.Add(m_reviveAction);
+
+            var m_unlockAction = new zPlayerBotActionUnlock.Descriptor(this.m_bot);
+            this.m_unlockAction = m_unlockAction;
+            data.allActions.Add(m_unlockAction);
+
+            var m_highlightAction = new zPlayerBotActionHighlight.Descriptor(this.m_bot);
+            this.m_highlightAction = m_highlightAction;
+            data.allActions.Add(m_highlightAction);
+
+            var m_useEnemyScannerAction = new zPlayerBotActionUseEnemyScanner.Descriptor(this.m_bot);
+            this.m_useEnemyScannerAction = m_useEnemyScannerAction;
+            data.allActions.Add(m_useEnemyScannerAction);
+
+            var m_tagEnemiesAction = new zPlayerBotActionUseEnemyScanner.Descriptor(this.m_bot); // resued same class for some reason.  Create another one?
+            this.m_tagEnemiesAction = m_tagEnemiesAction;
+            data.allActions.Add(m_tagEnemiesAction);
+
+            var m_collectItemAction = new zPlayerBotActionCollectItem.Descriptor(this.m_bot);
+            this.m_collectItemAction = m_collectItemAction;
+            data.allActions.Add(m_collectItemAction);
+
+            var m_shareResourceAction = new zPlayerBotActionShareResourcePack.Descriptor(this.m_bot);
+            this.m_shareResourceAction = m_shareResourceAction;
+            data.allActions.Add(m_shareResourceAction);
+
+            var m_evadeAction = new zPlayerBotActionEvadeProjectile.Descriptor(this.m_bot);
+            m_evadeAction.PrioLook = RootPlayerBotAction.m_prioSettings.EvadeProjectileLook;
+            m_evadeAction.PrioPrecaution = RootPlayerBotAction.m_prioSettings.EvadeProjectilePrecaution;
+            this.m_evadeAction = m_evadeAction;
+            data.allActions.Add(m_evadeAction);
+
+        }
+    }
+}
--- zRootBotPlayerAction/zRootActionRewritePatch.cs
+++ zRootBotPlayerAction/zRootActionRewritePatch.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using Il2CppSystem.Security.Cryptography;
 using Player;
 using System;
 using System.Collections.Generic;
@@ -11,82 +12,29 @@ namespace ZombieTweak2.zRootBotPlayerAction
     public class RootPlayerBotActionData
     {
         public List<ICustomPlayerBotActionBase.IDescriptor> allActions = new();
-        public bool constructed = false;
     }
     [HarmonyPatch]
     public class RootActionRewritePatch
     {
-        [HarmonyPatch(typeof(RootPlayerBotAction), MethodType.Constructor, new Type[] { typeof(RootPlayerBotAction.Descriptor) })]
-        [HarmonyPrefix]
-        public static bool RootCtor(RootPlayerBotAction __instance, RootPlayerBotAction.Descriptor desc)
+        internal static RootPlayerBotActionData GetOrCreateData(PlayerBotActionBase botBase)
         {
-            return false;
+            int botId = botBase.m_bot.GetInstanceID();
+            if (!RootActionDataStore.TryGetValue(botId, out var data))
+            {
+                data = new RootPlayerBotActionData();
+                RootActionDataStore[botId] = data;
+            }
+            return data;
         }
-        private static readonly ConditionalWeakTable<RootPlayerBotAction, RootPlayerBotActionData> RootActionDataStore = new();
-        public static void Construct(RootPlayerBotAction __instance)
+        internal static readonly Dictionary<int, RootPlayerBotActionData> RootActionDataStore = new();
+
+        [HarmonyPatch(typeof(RootPlayerBotAction.Descriptor), nameof(RootPlayerBotAction.Descriptor.CreateAction))]
+        [HarmonyPrefix]
+        public static bool CreateAction(RootPlayerBotAction.Descriptor __instance,ref PlayerBotActionBase __result)
         {
-            // We can't patch the real Construct, so we do this instead.
-            // initalize our custom data
-            var data = RootActionDataStore.GetValue(__instance, _ => new RootPlayerBotActionData());
-            data.constructed = true;
-            data.allActions.Clear();
-
-            //hurt action is handled somewhere else?  Might need to find it?  Or maybe it's unused now?  Who knows.
-
-            var m_idleAction = new zPlayerBotActionIdle.Descriptor(__instance.m_bot); // Create the action wrapper
-            m_idleAction.PrioFreezeForTwitcher = RootPlayerBotAction.m_prioSettings.IdleFreezeForTwitcher;
-            m_idleAction.PrioFreezeForInteraction = RootPlayerBotAction.m_prioSettings.IdleFreezeForInteraction;
-            m_idleAction.PrioLook = RootPlayerBotAction.m_prioSettings.IdleLook;
-            m_idleAction.PrioPrepareAction = RootPlayerBotAction.m_prioSettings.IdlePrepare;
-            __instance.m_idleAction = m_idleAction; // Assign it to the instance
-            data.allActions.Add(m_idleAction); // Add it to our custom list
-            // Repeat
-            var m_followLeaderAction = new zPlayerBotActionFollow.Descriptor(__instance.m_bot);
-            __instance.m_followLeaderAction = m_followLeaderAction;
-            data.allActions.Add(m_followLeaderAction);
-
-            var m_useBioscanAction = new zPlayerBotActionUseBioscan.Descriptor(__instance.m_bot);
-            __instance.m_useBioscanAction = m_useBioscanAction;
-            data.allActions.Add(m_useBioscanAction);
-
-            var m_attackAction = new zPlayerBotActionAttack.Descriptor(__instance.m_bot);
-            __instance.m_attackAction = m_attackAction;
-            data.allActions.Add(m_attackAction);
-
-            var m_reviveAction = new zPlayerBotActionRevive.Descriptor(__instance.m_bot);
-            __instance.m_reviveAction = m_reviveAction;
-            data.allActions.Add(m_reviveAction);
-
-            var m_unlockAction = new zPlayerBotActionUnlock.Descriptor(__instance.m_bot);
-            __instance.m_unlockAction = m_unlockAction;
-            data.allActions.Add(m_unlockAction);
-
-            var m_highlightAction = new zPlayerBotActionHighlight.Descriptor(__instance.m_bot);
-            __instance.m_highlightAction = m_highlightAction;
-            data.allActions.Add(m_highlightAction);
-
-            var m_useEnemyScannerAction = new zPlayerBotActionUseEnemyScanner.Descriptor(__instance.m_bot);
-            __instance.m_useEnemyScannerAction = m_useEnemyScannerAction;
-            data.allActions.Add(m_useEnemyScannerAction);
-
-            var m_tagEnemiesAction = new zPlayerBotActionUseEnemyScanner.Descriptor(__instance.m_bot); // resued same class for some reason.  Create another one?
-            __instance.m_tagEnemiesAction = m_tagEnemiesAction;
-            data.allActions.Add(m_tagEnemiesAction);
-
-            var m_collectItemAction = new zPlayerBotActionCollectItem.Descriptor(__instance.m_bot);
-            __instance.m_collectItemAction = m_collectItemAction;
-            data.allActions.Add(m_collectItemAction);
-
-            var m_shareResourceAction = new zPlayerBotActionShareResourcePack.Descriptor(__instance.m_bot);
-            __instance.m_shareResourceAction = m_shareResourceAction;
-            data.allActions.Add(m_shareResourceAction);
-
-            var m_evadeAction = new zPlayerBotActionEvadeProjectile.Descriptor(__instance.m_bot);
-            m_evadeAction.PrioLook = RootPlayerBotAction.m_prioSettings.EvadeProjectileLook;
-            m_evadeAction.PrioPrecaution = RootPlayerBotAction.m_prioSettings.EvadeProjectilePrecaution;
-            __instance.m_evadeAction = m_evadeAction;
-
-            data.allActions.Add(m_evadeAction);
+            // Can't hook the constructor directly for some reason, so hook the method that calls the constructor and call my wrapper instead.
+            __result = new zRootPlayerBotAction(__instance);
+            return false;
         }
 
         [HarmonyPatch(typeof(RootPlayerBotAction), nameof(RootPlayerBotAction.Update))]
@@ -95,23 +43,19 @@ namespace ZombieTweak2.zRootBotPlayerAction
         {
             if (!ZiMain.newRootBotPlayerAction)
                 return true;
-            var data = RootActionDataStore.GetValue(__instance, _ => new RootPlayerBotActionData());
-            if (!data.constructed)
-            {
-                Construct(__instance);
-            }
+            var data = GetOrCreateData(__instance);
             return true;
-            __result = __instance.IsActive();
-            //if (base.Update())
-            var baseUpdate = AccessTools.Method(typeof(PlayerBotActionBase), "Update");
-            bool baseResult = (bool)baseUpdate.Invoke(__instance, null);
-            if (baseResult)
-            {
-                __result = true;
-                return false; // skip original
-            }
-            return false;
         }
 
     }
 }
+//__result = __instance.IsActive();
+////if (base.Update())
+//var baseUpdate = AccessTools.Method(typeof(PlayerBotActionBase), "Update");
+//bool baseResult = (bool)baseUpdate.Invoke(__instance, null);
+//if (baseResult)
+//{
+//    __result = true;
+//    return false; // skip original
+//}
+//return false;
