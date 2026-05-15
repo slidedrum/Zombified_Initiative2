3115c10e4e01befbee431603b560c0ba2acc330a
SlideDrum <slidedrumofficial@gmail.com>
2026-05-15 17:49:35 -0400

    Added option to restrict what bots will ping

---
+++ CustomActions/Patches/HighlightActionPatch.cs
@@ -0,0 +1,62 @@
+﻿using HarmonyLib;
+using Player;
+using SlideMenu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZombieTweak2.CustomActions.Patches
+{
+    [HarmonyPatch]
+    public class HighlightActionPatch
+    {
+        private static PlayerBotActionBase.Descriptor originalBestAction;
+        public static Dictionary<PlayerBotActionHighlight.Descriptor.TargetTypeEnum, bool> targetTypePerms = new()
+        {
+            {PlayerBotActionHighlight.Descriptor.TargetTypeEnum.Container, true },
+            {PlayerBotActionHighlight.Descriptor.TargetTypeEnum.Door, true },
+            {PlayerBotActionHighlight.Descriptor.TargetTypeEnum.Terminal, true },
+        };
+        public static Dictionary<PlayerBotActionHighlight.Descriptor.TargetTypeEnum, sMenu.sMenuNode> nodes = new()
+        {
+            {PlayerBotActionHighlight.Descriptor.TargetTypeEnum.Container, AutomaticActionMenuClass.PingMenuClass.containersNode },
+            {PlayerBotActionHighlight.Descriptor.TargetTypeEnum.Door, AutomaticActionMenuClass.PingMenuClass.doorsNode },
+            {PlayerBotActionHighlight.Descriptor.TargetTypeEnum.Terminal, AutomaticActionMenuClass.PingMenuClass.terminalsNode },
+        };
+        public static void ToggleTargetTypePerms(PlayerBotActionHighlight.Descriptor.TargetTypeEnum targetType)
+        {
+            bool allowed = !targetTypePerms[targetType];
+            SetTargetTypePerms(targetType, allowed);
+        }
+        public static void SetTargetTypePerms(PlayerBotActionHighlight.Descriptor.TargetTypeEnum targetType, bool allowed)
+        {
+            targetTypePerms[targetType] = allowed;
+            var node = nodes[targetType];
+            if (allowed)
+                node.SetColor(sMenuManager.defaultColor);
+            else
+                node.SetColor(new UnityEngine.Color(0.25f, 0f, 0f));
+        }
+        [HarmonyPatch(typeof(RootPlayerBotAction), nameof(RootPlayerBotAction.UpdateActionHighlight))]
+        [HarmonyPrefix]
+        public static void PreUpdateActionHighlight(RootPlayerBotAction __instance, ref PlayerBotActionBase.Descriptor bestAction)
+        {
+            originalBestAction = bestAction;
+        }
+        [HarmonyPatch(typeof(RootPlayerBotAction), nameof(RootPlayerBotAction.UpdateActionHighlight))]
+        [HarmonyPostfix]
+        public static void PostUpdateActionHighlight(RootPlayerBotAction __instance, ref PlayerBotActionBase.Descriptor bestAction)
+        {
+            if (bestAction == null)
+                return;
+            if (bestAction.TryCast<PlayerBotActionHighlight.Descriptor>() == null)
+                return;
+            var targetType = __instance.m_highlightAction.TargetType;
+            if (!targetTypePerms[targetType])
+                bestAction = originalBestAction;
+        }
+
+    }
+}
--- zMenus.cs
+++ zMenus.cs
@@ -428,6 +428,9 @@ namespace ZombieTweak2
         {
             public static sMenu pingMenu;
             public static sMenu.sMenuNode pingNode;
+            public static sMenu.sMenuNode doorsNode;
+            public static sMenu.sMenuNode containersNode;
+            public static sMenu.sMenuNode terminalsNode;
             public static void Setup(sMenu menu)
             {
                 pingMenu = menu;
@@ -435,7 +438,12 @@ namespace ZombieTweak2
                 pingNode.ClearListeners(sMenuManager.nodeEvent.OnUnpressedSelected);
                 pingNode.AddListener(sMenuManager.nodeEvent.OnTapped, togglePingPerms);
                 pingNode.AddListener(sMenuManager.nodeEvent.OnDoubleTapped, pingMenu.Open);
+
+                doorsNode = pingMenu.AddNode("Doors", HighlightActionPatch.ToggleTargetTypePerms, PlayerBotActionHighlight.Descriptor.TargetTypeEnum.Door);
+                containersNode = pingMenu.AddNode("Containers", HighlightActionPatch.ToggleTargetTypePerms, PlayerBotActionHighlight.Descriptor.TargetTypeEnum.Container);
+                terminalsNode = pingMenu.AddNode("Terminals", HighlightActionPatch.ToggleTargetTypePerms, PlayerBotActionHighlight.Descriptor.TargetTypeEnum.Terminal);
             }
+
             public static void togglePingPerms()
             {
                 bool allowed = true;
